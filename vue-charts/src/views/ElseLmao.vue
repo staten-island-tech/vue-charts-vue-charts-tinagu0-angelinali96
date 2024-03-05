2dc6e456d02d10e2fafe2a5174038ea044e5eb4c
TinaGu0 <tinaguo0904@gmail.com>
2024-03-05 10:19:09 -0500

    a

---
+++ vue-charts/src/views/ElseLmao.vue
@@ -0,0 +1,33 @@
+<template>
+    <Bar
+      id="my-chart-id"
+      :options="chartOptions"
+      :data="chartData"
+    />
+  </template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { data } from './data'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+    export default {
+        components: { Bar },
+        setup(){
+            return {
+            chartData: {
+                labels: [ 'January', 'February', 'March', 'April', 'May' ],
+                datasets: data
+            },
+            chartOptions: {
+                responsive: true
+        }
+    }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue-charts/src/views/data.js
@@ -0,0 +1,3 @@
+const data = [ { data: [40, 20, 12, 20, 90] } ]
+
+export { data }
