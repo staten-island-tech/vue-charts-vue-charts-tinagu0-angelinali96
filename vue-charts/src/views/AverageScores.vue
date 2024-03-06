9469c89c062a1b84bf953f90ca9221c150b7780f
angelinali96 <136626552+angelinali96@users.noreply.github.com>
2024-03-06 09:47:36 -0500

    ???????????????????

--- vue-charts/src/views/AverageScores.vue
+++ vue-charts/src/views/AverageScores.vue
@@ -6,34 +6,7 @@
 
 <script>
 import { apiData } from '@/stores/fetchapi';
-import { Bar } from 'vue-chartjs';
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
-const boroughs = {
-  'Manhattan': 'M',
-  'Bronx': 'X',
-  'Brooklyn': 'K',
-  'Queens': 'Q',
-  'Staten Island': 'R'
-};
-    export default {
-        name: 'AverageScores',
-        components: {Bar}, 
-        data(){
-            return {
-      chartData: {
-        labels: Object.keys(boroughs),
-        datasets: [ { data: [[40, 20],[30, 10],[10, 8],[20, 40],[40, 20]] , 
-            backgroundColor: '#FFF'} ],
-       
-      },
-      chartOptions: {
-        responsive: true
-      }
-    }
-        }
-    }
 </script>
 
 <style scoped>
