ea74a493b831c7b0d4fa95d263832226e347b5c5
TinaGu0 <tinaguo0904@gmail.com>
2024-03-06 09:46:20 -0500

    a

--- vue-charts/src/App.vue
+++ vue-charts/src/App.vue
@@ -3,11 +3,9 @@
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/testtakers">Test Takers</RouterLink>
         <RouterLink to="/avgscores">Average Scores</RouterLink>
       </nav>
