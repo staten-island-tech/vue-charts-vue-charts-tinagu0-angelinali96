0043ff0e6ee089347ae6bc499b586b7444158b18
angelinali96 <136626552+angelinali96@users.noreply.github.com>
2024-03-08 10:11:58 -0500

    show average number of test takers

--- vue-charts/src/views/TestTakers.vue
+++ vue-charts/src/views/TestTakers.vue
@@ -5,19 +5,14 @@
   </div>
 </template>
 <script>
-import {ref, onMounted} from 'vue';
-// import { apiData } from '@/stores/fetchapi';
+import {ref} from 'vue';
 import { Bar } from 'vue-chartjs';
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
-/* const dataaa = ref('');
-async function getsgsdg(){
-  dataaa.value = data;
-} */
-
 let {apiData} = ref({});
-let boroughData = ref({});
+let boroughMinMax = ref([]);
+let boroughAvg = ref([]);
 function filterByBorough(areacode){ // categorize data by borough
   let filteredData = apiData.filter(item => item.dbn.includes(areacode) === true);
   return filteredData.filter(item => item.num_of_sat_test_takers.includes('s') != true); // eliminate non number values
@@ -28,8 +23,15 @@ function sortByTestTaker(a, b) { // sort data in increasing order by number of t
 function returnExtrema(arr){ // get highest and lowest number for floating bar chart
   return [Number(arr[0].num_of_sat_test_takers), Number(arr[arr.length-1].num_of_sat_test_takers)];
 }
-function dataToUse(borough){ // reduce length of function in object bc clearly i cant organize anything
+function dataMinMax(borough){ // reduce length of function in object 
   return returnExtrema(filterByBorough(borough).sort(sortByTestTaker))
+}
+
+function avgNumPpl(borough){ // get average number of test takers by borough
+  let numbers = filterByBorough(borough);
+  let total = 0;
+  numbers.forEach(item => total = total+Number(item.num_of_sat_test_takers));
+  return total/(numbers.length);
 }
 
     export default {
@@ -37,27 +39,54 @@ function dataToUse(borough){ // reduce length of function in object bc clearly i
         components: {Bar}, 
         data: ()=>({
           loaded: false,
-          chartdata: null
+          chartdata: null,
+          chartOptions: {
+            responsive: true
+          },
+          scales:{
+            y:{
+              stacked: true
+            }
+          }
         }),
   async mounted () {
     this.loaded = false
     try {
-      const response = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json');
+      const response = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json'); // get api
       apiData = await response.json();
-      boroughData = {
-        manhattan: dataToUse('M'),
-        bronx: dataToUse('X'),
-        brooklyn: dataToUse('K'),
-        queens: dataToUse('Q'),
-        statenisland: dataToUse('R'),
-      }; 
+      boroughMinMax = [
+        dataMinMax('M'),
+        dataMinMax('X'),
+        dataMinMax('K'),
+        dataMinMax('Q'),
+        dataMinMax('R'),
+    ]; 
+    boroughAvg = [
+        avgNumPpl('M'),
+        avgNumPpl('X'),
+        avgNumPpl('K'),
+        avgNumPpl('Q'),
+        avgNumPpl('R'),
+    ];
       const chartdata = {
         labels: ['Manhattan', 'Bronx', 'Brooklyn', 'Queens', 'Staten Island'],
-        datasets: [{data: [boroughData.manhattan,boroughData.bronx,boroughData.brooklyn,boroughData.queens,boroughData.statenisland],
-      backgroundColor: 'rgb(200,200,200)',label: 'Highest and Lowest Number of Test Takers'}],
+        datasets: [
+          {
+            data: boroughMinMax,
+            backgroundColor: 'rgb(200,200,200)',
+            label: 'Highest and Lowest Number of Test Takers',
+            type: 'bar'
+          },
+          {
+            data: boroughAvg,
+            backgroundColor: 'rgb(250,200,200)',
+            label: 'Average Test Takers',
+            type: 'bar'
+          }
+        ],
       };
       this.chartdata = chartdata;
-      this.loaded = true;      
+      this.loaded = true;  
     } catch (e) {
       console.error(e)
     }
@@ -66,11 +95,5 @@ function dataToUse(borough){ // reduce length of function in object bc clearly i
 
 </script>
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
