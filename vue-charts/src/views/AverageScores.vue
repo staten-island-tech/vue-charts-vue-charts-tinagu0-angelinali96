fd83ff5a339c06cf1d8918dd4c001d00a0fc3062
TinaGu0 <tinaguo0904@gmail.com>
2024-03-17 22:25:38 -0400

    almost done (yay will fix later )

--- vue-charts/src/views/AverageScores.vue
+++ vue-charts/src/views/AverageScores.vue
@@ -1,58 +1,80 @@
 <template>
-    <DropMenu :index="index" :scores="scores"/>
-    <Bar
-      id="my-chart-id"
-      :options="chartOptions"
-      :data="chartData"
-    />
+    <div>
+      <Dropdown v-model="selectedSchool" :options="schoolNames" placeholder="Select a School" aria-label="School Selector" class="w-full md:w-14rem" />
+      <Bar
+        id="my-chart-id"
+        :options="chartOptions"
+        :data="chartData"
+      />
+    </div>
   </template>
-
-<script>
-import DropMenu from '../components/DropMenu.vue'
-import { ref, onMounted } from 'vue';
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-import { a } from './data'
-
-const scores = ref('')
-async function getData() {
-    let res = await fetch("https://data.cityofnewyork.us/resource/f9bf-2cp4.json");
-    let data = await res.json();
-    scores.value = data;
-    // get list of schools and their array index for the dropdown
-    // get the scores for each category in the school with the parameter of index in array
-    // feed into graph
-};
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-    export default {
-        props: {
-            scores: Array,
-            index: Number,
-        },
-        components: { 
-            Bar, 
-            DropMenu,
-        },
-        setup(){
-            async () => {
-            await getData(); // Wait for getData() to get the data
-            console.log(scores.value);
-            };
-            return {
-            chartData: {
-                labels: [ 'January', 'February', 'March', 'April', 'May' ],
-                datasets: a
-            },
-            chartOptions: {
-                responsive: true
-            }, 
-        
-        }
+  
+  <script>
+  import Dropdown from 'primevue/dropdown';
+  import { ref, onMounted, computed, watch } from 'vue';
+  import { Bar } from 'vue-chartjs'
+  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+  
+  //puts data into here 
+  const scores = ref([]);
+  async function getData() {
+    try {
+      let res = await fetch("https://data.cityofnewyork.us/resource/f9bf-2cp4.json");
+      let data = await res.json();
+      scores.value = data;
+    } catch (error) {
+      console.error("massive skill issue", error);
     }
-};
-</script>
+  };
+  
+  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+  
+  export default {
+    props: {
+      index: Number,
+      scores: Array,
+    },
+    components: { 
+      Bar, 
+      Dropdown,
+    },
+    setup() {
+      // Fetch data when component is mounted
+      onMounted(async () => {
+        await getData(); // Wait for getData() to get the data
+        console.log(scores.value);
+      });
+      //info for the dropdown
+      const selectedSchool = ref('');
+      const schoolNames = computed(() => scores.value.map(school => school.school_name));
 
-<style scoped>
-
-</style>
+    watch(selectedSchool, () => {
+    let values = scores.value.filter((school) => school.school_name == selectedSchool.value);
+    console.log(values);
+    })
+    let values = scores.value.filter((school) => school.school_name == selectedSchool.value);
+    let scores = [
+        {
+        reading: values.sat_critical_reading_avg_score,
+        writing: values.sat_writing_avg_score,
+        math: values.sat_math_avg_score,
+        },
+    ];
+  
+      return {
+        selectedSchool,
+        schoolNames,
+        chartData: {
+          labels: ['Reading', 'Writing', 'Math'],
+          datasets: [{ data: scores, backgroundColor: 'rgb(200,200,200)' }]
+        },
+        chartOptions: {
+          responsive: true
+        },
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  </style>
